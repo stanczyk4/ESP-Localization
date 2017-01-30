98fc52472efcf499c7101d0f9172e5a519db5b66
Michael Stanczyk <Michael Stanczyk>
2017-01-29 23:20:53 -0600

    Added ESP8266 WiFi Scanner. Arduino Project. This is meant to serve as a fake sensor to test the localization rather than using an android smartphone. This project scans for available networks, prints its information over serial. Use this to run code on a laptop with the triangulation method that takes this serial data and locates the object.

---
+++ ESP-WifiScanner/ESP-WifiScanner.ino
@@ -0,0 +1,47 @@
+#include <ESP8266WiFi.h>
+
+//define WiFi connection information
+//current not connecting to a network, so commented out
+//const char* ssid = "";
+//const char* password = "";
+
+#define SCAN_PERIOD  5000
+
+bool scanReady = true;
+long lastScanMillis;
+
+void setup() {
+    //Setup serial communication
+    Serial.begin(115200);
+    delay(100);
+    Serial.println("Connected to Serial Interface");
+
+    //initialize Wifi, but disconnect so its not doing anything
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+}
+
+void loop() {
+    //if (millis() - lastScanMillis > SCAN_PERIOD){
+    //    WiFi.scanNetworks(true);
+    //    lastScanMillis = millis();
+    //}
+    if (scanReady){
+        WiFi.scanNetworks(true);
+        scanReady = false;
+    }
+    int n = WiFi.scanComplete();    //checks if the previous run scannetworks function has completed. (runs asychronous). returns -1 if not done.
+    //if scan is complete, print information for each network
+    if(n >= 0){
+        Serial.printf("%d network(s) found\n", n);
+        for (int i = 0; i < n; i++){
+            Serial.printf("%d: %s, Ch:%d (%ddBm) %s\n", i+1, WiFi.SSID(i).c_str(), WiFi.channel(i), WiFi.RSSI(i), WiFi.encryptionType(i) == ENC_TYPE_NONE ? "open" : ""); //print network information
+        }
+        WiFi.scanDelete(); //clears the scanning result from memory to prevent repeating data
+        Serial.printf("\n Scan Complete.\n");
+        scanReady = true;
+        Serial.printf("%ld milliseconds to complete Scan.",millis()-lastScanMillis);
+        lastScanMillis = millis(); 
+    }
+}
