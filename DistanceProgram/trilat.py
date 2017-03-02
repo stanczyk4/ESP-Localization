e648549cec3f54477f5f2ba9ed2eeaf747dd133b
Michael Stanczyk <stanczyk4@gmail.com>
2017-03-02 11:25:49 -0600

    fixed syntax errors

--- DistanceProgram/trilat.py
+++ DistanceProgram/trilat.py
@@ -2,17 +2,20 @@ import localization as lx
 
 def trilat2D(circles):
     mySolver = "LSE"
-    if len(circles == 2):
+    if len(circles) == 2:
     	mySolver = "LSE_GC"
-    elif len(circles < 2):
+    elif len(circles) < 2:
     	return -1
 
     P = lx.Project(mode="2D", solver=mySolver)
- 	t, label = P.add_target()
+ 	
 
     for anchors in circles:
-    	P.add_anchor(anchors.mac, (anchor.x, anchor.y))
-    	t.add_measure(anchors.mac, anchors.r)
+    	P.add_anchor(anchors.mac, (anchors.x, anchors.y))
+    	
+    t, label = P.add_target()
+    for anchors in circles:
+        t.add_measure(anchors.mac, anchors.r)
 
     P.solve()
     center = t.loc
