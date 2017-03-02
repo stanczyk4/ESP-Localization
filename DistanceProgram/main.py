e08bbfadc998dc94699e361279bf5ac2070dc274
Michael Stanczyk <stanczyk4@gmail.com>
2017-03-02 10:46:23 -0600

    Updated main.py to use new trilat.py method, as well as handle if we have less than 2 nodes.

--- DistanceProgram/main.py
+++ DistanceProgram/main.py
@@ -22,25 +22,29 @@ def main ():
 
     for i in circles:
         print i
-        print "\n\n"
+        print "\n"
 
     # PASS CIRCLES LIST HERE
 
 
     fig=plt.figure(1)
-    plt.axis([-6,6,-6,6])
+    plt.axis([-5,10,-5,10])
     ax=fig.add_subplot(1,1,1)
     plt.ion()
     plt.show()
     ax.cla()
     plot.drawCircles(plt, ax, circles)
     
-    #predictedPosition = trilat.trilat2D(circles)
+    predictedPosition = trilat.trilat2D(circles)
+    if (predictedPosition == -1):
+        print("Need 2 or more nodes")
+        raw_input("Press enter to continue...")
 
-    #ax.add_patch(plt.Circle((predictedPosition.x, predictedPosition.y),radius=.05, color='b', fill=True))
-    #plt.draw()
-    #print predictedPosition.x, predictedPosition.y
-    input("Press enter to continue...")
+    ax.add_patch(plt.Circle((predictedPosition.x, predictedPosition.y),radius=.05, color='b', fill=True))
+    plt.draw()
+    print predictedPosition.x, predictedPosition.y
+
+    raw_input("Press enter to continue...")
 
 main()
 
