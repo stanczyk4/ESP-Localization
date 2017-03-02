5f8084ad9c44603f73ac11709a08545b51794a38
Michael Stanczyk <Michael Stanczyk>
2017-03-01 22:28:50 -0600

    edited to include trilat and plot functions

--- DistanceProgram/main.py
+++ DistanceProgram/main.py
@@ -1,24 +1,44 @@
 import time
 import scanner
 import identify
+import trilat
+import plot
+import matplotlib.pyplot as plt
 
-test, distances = scanner.get_BSSI()
-distances = sorted(distances)
-for i in range(0,10):
-    # Wait
-    time.sleep(3)
+def main ():
 
-    # Save old test and distances
-    oldTest = test
-    oldDistances = distances
-
-    # Scan again
     test, distances = scanner.get_BSSI()
-
-    # Sort the list of distances
     distances = sorted(distances)
-    circles = identify.identify(distances)
+    for i in range(0,10):
+        # Wait
+        time.sleep(3)
 
-    # PASS CIRCLES LIST HERE
+        # Save old test and distances
+        oldTest = test
+        oldDistances = distances
 
-input("Press enter to continue...")
+        # Scan again
+        test, distances = scanner.get_BSSI()
+
+        # Sort the list of distances
+        distances = sorted(distances)
+        circles = identify.identify(distances)
+
+        # PASS CIRCLES LIST HERE
+
+
+    fig=plt.figure(1)
+    plt.axis([-6,6,-6,6])
+    ax=fig.add_subplot(1,1,1)
+    plt.ion()
+    plt.show()
+    ax.cla()
+
+    finalPosition = trilat2D(circles);
+
+    ax.add_patch(plt.Circle((finalPosition.x, finalPosition.y),radius=.01, color='b', fill=True))
+    plt.draw()
+    input("Press enter to continue...")
+
+main()
+
