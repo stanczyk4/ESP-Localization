36c3d286cb0e144c10163fddbeccfe91186fc1c9
Nick P <nicholasgpratt@gmail.com>
2017-03-02 18:59:38 -0600

    Updated to include 2 extra MAC addresses

--- DistanceProgram/definitions.py
+++ DistanceProgram/definitions.py
@@ -1,7 +1,16 @@
 locations = {'5E:CF:7F:D1:75:2A':[-1,2], '5E:CF:7F:D1:78:FE':[2,3]}
 
+
+#                   4                               1                           3                               2
+
 # Node 1
 # 5E:CF:7F:D1:78:FE
+
+# Node 2
+# 5E:CF:7F:D1:8F:87
+
+# Node 3
+# 5E:CF:7F:D1:83:81
 
 # Node 4
 # 5E:CF:7F:D1:75:2A
