ed8664078278d174227ec5eb6763c112b77dcad9
Michael Stanczyk <Michael Stanczyk>
2017-03-02 18:30:57 -0600

    Fixed main function if else statement to only plot when we have enough nodes to graph something. Otherwise dont try and calculate predicted position and graph nodes, and instead inform the user that we need 2 or more nodes.

--- DistanceProgram/main.py
+++ DistanceProgram/main.py
@@ -17,40 +17,34 @@ def main ():
 
     # Returns a list of circle objects (MAC,x,y,r)
     circles = identify.identify(nodes)
-    
-
 
     for i in circles:
         print i
         print "\n"
 
-    # PASS CIRCLES LIST HERE
-
-    # Figure setup
-    fig=plt.figure(1)
-    plt.axis([-5,10,-5,10])
-    ax=fig.add_subplot(1,1,1)
-    plt.ion()
-    plt.show()
-    ax.cla()
-
-    # Draw the found nodes and their estimated distances
-    plot.drawCircles(plt, ax, circles)
-    
-    # Try to predict a central position
-    predictedPosition = trilat.trilat2D(circles)
-
     # If we don't have 2 or more nodes, the trilateration function will not work
     if (len(circles)<2):
         # Alert the user
         print("Need 2 or more nodes")
-        raw_input("Press enter to continue...")
-    # Otherwise draw the predicted position on the graph
+
     else:
+        # Try to predict a central position
+        predictedPosition = trilat.trilat2D(circles)
+        # Figure setup
+        fig=plt.figure(1)
+        plt.axis([-5,10,-5,10])
+        ax=fig.add_subplot(1,1,1)
+        plt.ion()
+        plt.show()
+        ax.cla()
+
+        # Otherwise draw the predicted position on the graph
+        # Draw the found nodes and their estimated distances
+        plot.drawCircles(plt, ax, circles)
         ax.add_patch(plt.Circle((predictedPosition.x, predictedPosition.y),radius=.05, color='b', fill=True))
-    # Show the plot and print the predicted position
-    plt.show()
-    print predictedPosition.x, predictedPosition.y
+        print predictedPosition.x, predictedPosition.y
+        # Show the plot and print the predicted position
+        plt.show()
 
     raw_input("Press enter to continue...")
 
