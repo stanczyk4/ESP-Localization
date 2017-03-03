794da1e0e0c11bfbcdc9da339294f69783c55eed
Michael Stanczyk <stanczyk4@gmail.com>
2017-03-02 20:38:15 -0600

    Moved plot stuff into plot.py to make main.py more organized

--- DistanceProgram/main.py
+++ DistanceProgram/main.py
@@ -3,11 +3,11 @@ import scanner
 import identify
 import trilat
 import plot
-import matplotlib.pyplot as plt
 import distance
 
 def main ():
     plotGraphs = True
+
     nodes = []
     # Returning list of dictionaries where each is {BSSID:RSSI}
     print("Hit ctrl+c to stop scanning...\nScanning Networks...")
@@ -34,27 +34,12 @@ def main ():
     else:
          # Try to predict a central position
         predictedPosition = trilat.trilat2D(circles)
-        print predictedPosition.x, predictedPosition.y
+        error = math.sqrt(predictedPosition.x**2 + predictedPosition.y**2)
+        #print the predicated position and its error
+        print "x: ", predictedPosition.x, "y: ", predictedPosition.y, "\nError in meters: ", error, "\n"
+
         if (plotGraphs == True):
-            minX = min(i.x for i in circles)
-            minY = min(i.y for i in circles)
-            maxX = max(i.x for i in circles)
-            maxY = max(i.y for i in circles)
-            #axisOffset = max(i.r for i in circles)
-            axisOffset = 2;
-            # Figure setup
-            fig=plt.figure(1)
-            plt.axis([minX - axisOffset, maxX + axisOffset, minY - axisOffset, maxY + axisOffset])
-            ax=fig.add_subplot(1,1,1)
-            plt.ion()
-            plt.show()
-            ax.cla()
-            # Otherwise draw the predicted position on the graph
-            # Draw the found nodes and their estimated distances
-            plot.drawCircles(plt, ax, circles)
-            ax.add_patch(plt.Circle((predictedPosition.x, predictedPosition.y),radius=.05, color='b', fill=True))
-            # Show the plot and print the predicted position
-            plt.show()
+            plot.drawCircles(circles, predictedPosition)
 
     input = raw_input("Enter 1 to re-run, else quit program: ")
     if (input == '1'):
