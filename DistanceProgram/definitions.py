d3b753a7a9df0bd3e74686b0186629e4179045b5
Michael Stanczyk <stanczyk4@gmail.com>
2017-03-07 11:08:00 -0600

    added alternate user input method for node definitions. Commented out as to not interfere with old method, but keeping there to discuss its use later

--- DistanceProgram/definitions.py
+++ DistanceProgram/definitions.py
@@ -21,18 +21,33 @@ node5 = nodeClass("SPS:5", 'A2:20:A6:0D:0F:E8', 0, 0)
 # Node 6
 node6 = nodeClass("SPS:6", 'A2:20:A6:0D:0F:B9', 0, 0)
 # Node 7
-#node7 = node("Node 7", '', 0, 0)
+#node7 = node("SPS:7", '', 0, 0)
 # Node 8
-#node8 = node("Node 7", '', 0, 0)
+#node8 = node("SPS:8", '', 0, 0)
 
-nodeList.append(node1)
-nodeList.append(node2)
-nodeList.append(node3)
-nodeList.append(node4)
-nodeList.append(node5)
-nodeList.append(node6)
-
+#############LOOPING METHOD####################
 locations = {}
-
 for myNodes in nodeList:
     locations[myNodes.mac] = [myNodes.name,myNodes.x,myNodes.y]
+
+#############ALTERNATE METHOD USING LESS MEMORY, but maybe more user input?#############
+#to use this method comment out all the stuff above except class nodeClass
+#locations = {}
+# Node 1
+#nodeX = nodeClass("SPS:1", '5E:CF:7F:D1:78:FE', 0, 0)
+#locations.update({nodeX.mac:[nodeX.name,nodeX.x,nodeX.y]})
+# Node 2
+#nodeX = nodeClass("SPS:2", '5E:CF:7F:D1:8F:87', 0, 0)
+#locations.update({nodeX.mac:[nodeX.name,nodeX.x,nodeX.y]})
+# Node 3
+#nodeX = nodeClass("SPS:3", '5E:CF:7F:D1:83:81', 0, 0)
+#locations.update({nodeX.mac:[nodeX.name,nodeX.x,nodeX.y]})
+# Node 4
+#nodeX = nodeClass("SPS:4", '5E:CF:7F:D1:75:2A', 0, 0)
+#locations.update({nodeX.mac:[nodeX.name,nodeX.x,nodeX.y]})
+# Node 5
+#nodeX = nodeClass("SPS:5", 'A2:20:A6:0D:0F:E8', 0, 0)
+#locations.update({nodeX.mac:[nodeX.name,nodeX.x,nodeX.y]})
+# Node 6
+#nodeX = nodeClass("SPS:6", 'A2:20:A6:0D:0F:B9', 0, 0)
+#locations.update({nodeX.mac:[nodeX.name,nodeX.x,nodeX.y]})
